7d4d1b8595f98a3611cd0d9690bc7231212c4e53
joao vitor rangel villas boas <joaovitorrangelvillasboas@joaos-MacBook-Air.local>
2021-07-28 11:31:29 -0300

    Primeiro Commit

---
+++ etapas.js
@@ -0,0 +1,65 @@
+let etapas = [
+    {
+
+        titulo: 'VEREADOR',
+        numeros: 5,
+        canditados: [
+            {
+               numero: '38111',
+               nome: 'Fulano De Tal',
+               partido: 'ABC',
+               fotos: [
+                   {url:'Fotos/images.jpg', legenda:'Vereador', small: false} 
+               ]
+            },{
+                numero: '38112',
+                nome: 'Fulo De Tales',
+                partido: 'ABC',
+                fotos: [
+                    {url:'Fotos/teste.jpeg', legenda:'Vereador', small: false} 
+                ]
+             },
+        ]
+    },
+    {
+        numero: '77222',
+        nome: 'Ciclano Da Silva',
+        partido: 'DEFG',
+        fotos: [
+            {url:'Fotos/images.jpg', legenda: 'Vereador'}
+        ]
+    },
+{ 
+    titulo: 'PREFEITO',
+    numeros: 2,
+    canditados: [
+        {
+            numero: '99',
+            nome: 'Ciclano',
+            partido: 'ABC',
+            vice: 'Eu memo',
+            fotos: [
+                {url:'Fotos/images.jpg', legenda: 'Prefeito'},
+                {url:'Fotos/images.jpg', legenda: 'Vice-Prefeito',small: true}
+            ]
+     
+        },
+        {
+            numero: '84',
+            nome: 'Zulano',
+            partido: 'Vambora',
+            vice: 'Zulu',
+            fotos: [
+                {url:'Fotos/images.jpg', legenda:'Prefeito'},
+                {url:'Fotos/images.jpg', legenda:'Vice-Prefeito',small: true}
+            ]
+        
+        },
+    ]
+    
+
+    
+     }
+
+    
+]
